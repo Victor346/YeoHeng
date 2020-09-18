1d475541cebc12acee55bbef5d3f6920ae8a91c6
Fernando Rios <feriosch98@gmail.com>
2020-09-18 12:08:47 -0500

    Dummy events view in /events

---
+++ src/components/events/EventCard.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="column is-3">
+    <div class="card">
+      <div
+        class="card-image is-hidden-mobile"
+        style="margin-bottom: -20%"
+      >
+        <figure class="image is-16by9">
+          <img
+            class="desktop-image"
+            :src="event.imgUrl"
+            alt="Event image"
+          />
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left is-hidden-desktop">
+            <figure class="image is-48x48">
+              <img
+                :src="event.imgUrl"
+                alt="Event image"
+              />
+            </figure>
+          </div>
+          <div class="media-content">
+            <h1 class="title is-5">
+              {{event.title}} (${{event.price}})
+            </h1>
+            <h2 class="subtitle is-5">
+              {{event.city}}, {{event.country}}
+            </h2>
+          </div>
+        </div>
+        <div class="content has-text-left">
+          <p>
+            {{event.description}}
+          </p>
+          <b-taglist>
+            <b-tag v-for="tag in event.tags"
+                   :key="event.tags.indexOf(tag)"
+                   type="is-success"
+            >
+              {{tag}}
+            </b-tag>
+          </b-taglist>
+        </div>
+        <footer class="card-footer">
+          <p class="card-footer-item">
+            {{event.category}}
+          </p>
+          <p class="card-footer-item">
+            {{event.time}}
+          </p>
+        </footer>
+      </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+  props: {
+    event: {
+      title: String,
+      price: String,
+      city: String,
+      country: String,
+      time: String,
+      imgUrl: String,
+      description: String,
+      category: String,
+      tags: [String],
+    },
+  },
+};
+</script>
+
+<style scoped>
+.desktop-image {
+  mask-image: linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0));
+}
+</style>
---
+++ src/views/events/ViewEvent.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <section class="has-text-left">
+      <h1 class="title is-2">Mis eventos</h1>
+    </section>
+    <section>
+      <div class="columns" style="flex-wrap: wrap">
+        <EventCard v-for="event in events" :event="event" :key="events.indexOf(event)"/>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/events/EventCard.vue';
+
+export default {
+  name: 'ViewEvent',
+  data() {
+    return {
+      events: [
+        {
+          title: 'Titulo',
+          price: '2342',
+          city: 'city',
+          country: 'country',
+          time: '03:00',
+          imgUrl: 'https://yeoheng-itesm-2020.s3.amazonaws.com/temp/kum.jpg',
+          description: 'description',
+          category: 'category',
+          tags: ['tags', 'commie'],
+        },
+        {
+          title: 'Titulo 2',
+          price: '6666',
+          city: 'vule',
+          country: 'france',
+          time: '16:00',
+          imgUrl: 'https://yeoheng-itesm-2020.s3.amazonaws.com/Vc346/events/81fb2979-f290-4a0c-97a8-e32ff6673b29.jpg',
+          description: 'description 2',
+          category: 'cultural',
+          tags: ['tags', 'pepe', 'pepon'],
+        },
+      ],
+    };
+  },
+  components: {
+    EventCard,
+  },
+};
+</script>
+
+<style scoped>
+section {
+  padding-top: 1%;
+  padding-left: 2%;
+  padding-right: 2%;
+}
+</style>
